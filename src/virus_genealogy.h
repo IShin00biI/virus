aa71e8ec13e7b4309880758c8e813255be20e412
Michał Naruniec <mn360386@students.mimuw.edu.pl>
2016-12-15 04:06:26 +0100

    Zrobione to co wiadome bez systemu czyszczenia

--- src/virus_genealogy.h
+++ src/virus_genealogy.h
@@ -7,25 +7,48 @@
 #include <vector>
 
 class VirusAlreadyCreated : public std::exception {
-    const char*  what() const override {
+    virtual const char* what() const noexcept override {
         return "VirusAlreadyCreated";
     }
 };
 
 class VirusNotFound : public std::exception {
-    const char* what() const override {
+    virtual const char* what() const noexcept override {
         return "VirusNotFound";
     }
 };
 
 class TriedToRemoveStemVirus : public std::exception {
-    const char* what() const override {
+    virtual const char* what() const noexcept override {
         return "TriedToRemoveStemVirus";
     }
 };
 
 template <typename Virus>
 class VirusGenealogy {
+
+  private:
+    // Node in viruses' graph
+    class Node;
+
+    typedef typename Virus::id_type id_type;
+    typedef std::shared_ptr<Node> node_sh_ptr;
+    typedef std::weak_ptr<Node> node_w_ptr;
+    typedef std::map<id_type, node_sh_ptr> children_map;
+    typedef std::map<id_type, node_w_ptr> parents_map;
+    typedef parents_map global_map;
+    typedef typename children_map::iterator children_iterator;
+    typedef typename parents_map::iterator parents_iterator;
+    typedef typename global_map::iterator global_iterator;
+
+    // TODO ustalic kolejnosc, moze byc istotna dla kolejnosci destrukcji
+
+    // All nodes mapped to their viruses' ids
+    global_map nodes;
+
+    // Shared pointer to stem virus' node
+    node_sh_ptr stem_node_ptr;
+
   public:
 
     // Copy constructor deleted
@@ -41,9 +64,9 @@ class VirusGenealogy {
     id_type get_stem_id() const;
 
     //
-    std::vector<id_type> get_childen(const id_type& id) const;
+    std::vector<id_type> get_children(const id_type& id) const;
 
-    //
+
     std::vector<id_type> get_parents(const id_type& id) const;
 
     //
@@ -65,108 +88,153 @@ class VirusGenealogy {
     void remove(const id_type& id);
 
 
-  private:
-    typedef typename Virus::id_type id_type;
-    typedef std::shared_ptr<Node> node_sh_ptr;
-    typedef std::weak_ptr<Node> node_w_ptr;
-
-    // TODO ustalic kolejnosc, moze byc istotna dla kolejnosci destrukcji
-
-    // Node in viruses' graph
-    class Node;
-
-    // All nodes mapped to their viruses' ids
-    std::map<id_type, node_w_ptr> nodes;
-
-    // Shared pointer to stem virus' node
-    node_sh_ptr stem_node_ptr;
-
 
 
 };
 
+
+/*
+ * TODO ta klasa jest super istotna, zaleznie od memberow, konstruktorow i destruktorow
+ * wplywa na sposob pracy z wieloma funkcjami, mysle ze dobrze tu zrobione RAII moze zalatwic wiekszosc
+ * roboty, ale nie udalo mi sie go zrobic wedlug mojego planu.
+ * children, parents, value, get_id sa ok. Cala reszta do przerobienia
+ *
+ */
 template <typename Virus>
-class VirusGenealogy::Node {
+class VirusGenealogy<Virus>::Node {
     friend class VirusGenealogy<Virus>;
   public:
-    Node(const id_type& id)
+    /*
+    Node(const id_type& id, global_map& nodes)
         : value{id},
-          self_map_iter{}
+          nodes{nodes},
+          self_map_iter{this->nodes.end()}
     {}
 
-    // Moze rzucac wyjatkiem takim jak getId() wirusa, czyli ok
-    id_type getId() const {
-        return value->getId();
+    ~Node() {
+        if(self_map_iter != nodes.end()) nodes.erase(self_map_iter);
+    }*/
+
+    // Moze rzucac wyjatkiem takim jak get_id() wirusa, czyli ok
+    id_type get_id() const {
+        return value->get_id();
     }
   private:
     //TODO ustalic kolejnosc
 
     std::unique_ptr<Virus> value;
 
-    std::map<id_type, node_sh_ptr> children{};
+    children_map children{};
 
-    std::map<id_type, node_w_ptr> parents{};
+    parents_map parents{};
 
+    /*Probowalem zrobic w node'ach iteratory do swoich miejsc w global_mapie,
+     * tak jak mozna wnioskowac po konstruktorze i destruktorze, ale nie udalo sie,
+     * self_map_iter{this->nodes.end()} sie wywala. Trzeba sie zastanowic jak zrobic
+     * wyrzucanie z global_mapy(nie mozna tego zostawiac).
+     *
+    global_map& nodes;
 
     // Node's iterator in Genealogy's node map
-    std::map<id_type, node_w_ptr>::iterator self_map_iter;
+    global_iterator self_map_iter;
+     */
 };
 
 
-// Moze byc wyjatek braku pamieci, wowczas destruktory odpowiednich atrybutow wyczyszcza pamiec
+// TODO konstruktor grafu tez musi byc przemyslany, zeby wspolgral
+// z konstruktorem node'a i kolejnoscia konstrukcji
 template <typename Virus>
-VirusGenealogy::VirusGenealogy(const id_type& stem_id)
-    : nodes{},stem_node_ptr{std::make_shared<Node>(stem_id)}
+VirusGenealogy<Virus>::VirusGenealogy(const id_type& stem_id)
+    //: nodes{},
+      //stem_node_ptr{std::make_shared<Node>(stem_id, nodes)}
 {}
 
-// Moze rzucac wyjatkiem, patrz Node
+// Moze rzucac wyjatkiem, patrz Node(raczej gotowe)
 template <typename Virus>
-id_type VirusGenealogy::get_stem_id() const
-{
-    return stem_node_ptr->getId();
+typename Virus::id_type VirusGenealogy<Virus>::get_stem_id() const {
+    return stem_node_ptr->value->get_id();
 }
 
+// Jest silna odpornosc(raczej gotowe)
 template <typename Virus>
-std::vector<id_type> VirusGenealogy::get_childen(const id_type& id) const {
-
+std::vector<typename Virus::id_type>
+VirusGenealogy<Virus>::get_children(const id_type& id) const {
+    if(!exists(id)) throw VirusNotFound{};
+    children_map& children = nodes.at(id).lock()->children;
+    std::vector<id_type> result(children.size());
+    size_t result_index = 0;
+    for(children_iterator child = children.begin(); child != children.end(); ++child) {
+        result[result_index] = child->first;
+        ++result_index;
+    }
+    return std::move(result);
 }
 
+// Jest silna odporność. Usuwamy tu martwe weak_ptry.(raczej gotowe)
+// mozna pomyslec czy gdzies jeszcze czyscimy
 template <typename Virus>
-std::vector<id_type> VirusGenealogy::get_parents(const id_type& id) const {
+std::vector<typename Virus::id_type>
+VirusGenealogy<Virus>::get_parents(const id_type& id) const {
+    if(!exists(id)) throw VirusNotFound{};
+    parents_map& parents = nodes.at(id).lock()->parents;
+    std::vector<id_type> result;
+    result.reserve(parents.size());
+    parents_iterator parent = parents.begin();
+    while(parent != parents.end()) {
+        if(!parent->second.expired()) {
+            result.push_back(parent->first);
+            ++parent;
+        }
+        else parent = parents.erase(parent);
+        // CLionowi nie podoba sie powyzsza instrukcja, bo skasowalem operator= dla genealogii.
+        // Nie widze zwiazku, mozliwe ze bug IDE
+    }
 
+    return std::move(result);
 }
 
 // Mapa moze rzucac wyjatkiem, to nic
-bool VirusGenealogy::exists(const id_type& id) const {
+// TODO TA WERSJA DZIALA TYLKO PRZY USUWANIU Z GLOBAL_MAPY NA BIEZACO
+// MOCNO ZALEZY OD NASZYCH NIEZMIENNIKOW
+template <typename Virus>
+bool VirusGenealogy<Virus>::exists(const id_type& id) const {
     return nodes.count(id) != 0;
 }
 
-// Wyjatkiem moze rzucic exist, throw, mapa[].
-// lock() i * nie rzuca
+// Wyjatkiem moze rzucic exist, throw, at.
+// lock() i * nie rzuca(raczej gotowe)
 template <typename Virus>
-Virus& VirusGenealogy::operator[](const id_type& id) const {
+Virus& VirusGenealogy<Virus>::operator[](const id_type& id) const {
     if(!exists(id)) throw VirusNotFound{};
-    return *(nodes[id].lock());
+    return *(nodes.at(id).lock()->value);
 }
 
 //
-void VirusGenealogy::create(const id_type& id, const id_type& parent_id) {
+template <typename Virus>
+void VirusGenealogy<Virus>::create(const id_type& id, const id_type& parent_id) {
+    if(exists(id)) throw VirusAlreadyCreated{};
 
 }
 
-//
-void VirusGenealogy::create(const id_type& id, const std::vector<id_type>& parent_ids) {
+// TODO Trzeba sprawdzic informacje z forum o mozliwych wyjatkach w porownaniach, wplywaja na gwarancje map
+// TODO Jeden create powinien korzystac z drugiego.
+// TODO Trzeba sie zastanowic nad silna gwarancja, rowniez w zaleznosci od porownan.
+//Zwykly idiom pracy na kopii(temp&swap) wydaje sie byc ciezki, bo zmiany obejmuja rozne mapy
+template <typename Virus>
+void VirusGenealogy<Virus>::create(const id_type& id, const std::vector<id_type>& parent_ids) {
+    if(exists(id)) throw VirusAlreadyCreated{};
 
 }
 
-//
-void VirusGenealogy::connect(const id_type& child_id, const id_type& parent_id) {
+// TODO powinno nie byc trudne, ale watpie zeby sie przydalo do create
+template <typename Virus>
+void VirusGenealogy<Virus>::connect(const id_type& child_id, const id_type& parent_id) {
 
 }
 
-//
-void VirusGenealogy::remove(const id_type& id) {
-
+// TODO latwe z bezpiecznymi porownaniami, bo erase jest wtedy nothrow. Z niepewnymi jest gorzej..
+template <typename Virus>
+void VirusGenealogy<Virus>::remove(const id_type& id) {
 }
 
 
